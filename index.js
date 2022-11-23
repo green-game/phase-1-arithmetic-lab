f166411ac98d716fa0b013e41560aeaaa265348b
Allan Monzon <greengamer.am@gmail.com>
2022-11-22 20:03:43 -0500

    commit tree 08feb231401a58986a570a014757338f271ef904

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+var num1=2 ;
+var num2=31;
+var num3=8;
+var num4=2;
+var multiply = num1*num2
+    const random = Math.floor(Math.random() * 2)+1;
+console.log(random)
+var mod = num3 / num4
+var max = Math.max(0,20)
